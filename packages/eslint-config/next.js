91fbc8a70f69b09d1a4a61517b928fffc0cde237
Pedro Augusto B. Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-09-15 18:54:14 -0300

    chore: configuring the project

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,35 +1,9 @@
-const { resolve } = require("node:path");
-
-const project = resolve(process.cwd(), "tsconfig.json");
-
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
-  extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "turbo",
-  ],
-  globals: {
-    React: true,
-    JSX: true,
+  extends: ['@rocketseat/eslint-config/next'],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'no-useless-constructor': 'off',
   },
-  env: {
-    node: true,
-    browser: true,
-  },
-  plugins: ["only-warn"],
-  settings: {
-    "import/resolver": {
-      typescript: {
-        project,
-      },
-    },
-  },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
-};
+}
